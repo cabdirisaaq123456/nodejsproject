cb696cb4511f8a94aebda0c9f2d3f1fe2958fb5d
cabdirisaaq123456 <cabdirisaaqcalascali1@gmail.com>
2024-10-31 10:08:48 -0700

    file task roues add to desplay message unkown metod required

--- routes/taskroutes.js
+++ routes/taskroutes.js
@@ -1,16 +1,20 @@
 const taskroutes=(req,res) =>{
     if(req.method=='GET'){
-        res.end('get all tasks')
+        getTasks(req,res);
+        // res.end('get all tasks')
     }
     else if(req.method=='POST'){
-        res.end('create anew task!!!');
+        createTask(req,res)
+        // res.end('create anew task!!!');
     }
     else if(req.method == 'PATCH'){
-        res.end('update task');
+        // res.end('update task');
+        updateTask(req,res)
 
     }
     else if(req.method == 'DELETE'){
-        res.end('delete task');
+        deleteTask(req,res)
+        // res.end('delete task');
     }
     else{
         res.writehead(404,'not found',{'content_type':'application/json'
