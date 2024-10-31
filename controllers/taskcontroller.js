4942a157bdbc102ebde89df4e4932f22240c4568
cabdirisaaq123456 <cabdirisaaqcalascali1@gmail.com>
2024-10-31 10:43:22 -0700

    const incoming form in taskcontroller

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -1,5 +1,11 @@
+const{IncomingForm}= formidable=require('formidable')
 const { readTasksFromFile } = require("../utils/filehander")
 
 exports.getTasks=(req,res)=>{
   const task =readTasksFromFile();
+  res.writehead(200,{'content-type ':'application/json'})
+  res.end(JSON.stringify(task))
+}
+exports.createTask =(req,res) =>{
+
 }
--- routes/taskroutes.js
+++ routes/taskroutes.js
@@ -1,20 +1,16 @@
 const taskroutes=(req,res) =>{
     if(req.method=='GET'){
-        getTasks(req,res);
-        // res.end('get all tasks')
+        getTasks(req,res); 
     }
     else if(req.method=='POST'){
-        createTask(req,res)
-        // res.end('create anew task!!!');
+        createTask(req,res)  
     }
     else if(req.method == 'PATCH'){
-        // res.end('update task');
+      
         updateTask(req,res)
-
     }
     else if(req.method == 'DELETE'){
         deleteTask(req,res)
-        // res.end('delete task');
     }
     else{
         res.writehead(404,'not found',{'content_type':'application/json'
