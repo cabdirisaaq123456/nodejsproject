cad0415a255ea123f68e17cfd5c400762f05aa05
cabdirisaaq123456 <cabdirisaaqcalascali1@gmail.com>
2024-10-31 11:10:25 -0700

    const new tasks id,title,description,status,image in taskcontroller

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -1,5 +1,5 @@
 const{IncomingForm}= formidable=require('formidable')
-const { readTasksFromFile } = require("../utils/filehander")
+const { readTasksFromFile, writetaskTofile } = require("../utils/filehander")
 
 exports.getTasks=(req,res)=>{
   const task =readTasksFromFile();
@@ -7,5 +7,29 @@ exports.getTasks=(req,res)=>{
   res.end(JSON.stringify(task))
 }
 exports.createTask =(req,res) =>{
+  const form =new IncomingForm();
+  form.parse(req,(err,fields,files)=>{
+    if(err){
+      res.writehead(400,{'content-type':'application/json'});
+      res.end(JSON.stringify({
+       message:'error parsing form'
+      }))
+     return;
+    }
+
+    const tasks=readTasksFromFile()
+    const newTasks = {
+      id:Date.now(),
+      title:fields.title,
+      description:fields?.description || '',
+      status:fields ?.status||'pending',
+      Image:files.Image? '/uploads/${files.image.name}':null,
+    }
+    tasks.push(newTasks);
+    writetaskTofile(tasks);
+    if(files.Image){
+    copyfilesync(files.Image.path, '../uploads' +files.Image.name)
+    }
+  })
 
 }
